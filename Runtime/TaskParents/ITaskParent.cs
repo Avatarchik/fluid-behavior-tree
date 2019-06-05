218d604c0499287fc5cab92ac6a61db736a1d113
Ash Blue <ash@blueashes.com>
2019-06-04 18:20:39 -0600

    refactor(behavior-tree): tree is now generated from pure data

---
+++ Runtime/TaskParents/ITaskParent.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using CleverCrow.Fluid.BTs.Tasks;
+
+namespace CleverCrow.Fluid.BTs.TaskParents {
+    public interface ITaskParent : ITask {
+        ITaskParent AddChild (ITask child);
+    }
+}
